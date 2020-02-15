dfa7470093cf91ebcc19c7696f2d97bc3fc1d661
MaciejAmbroziak <maciejambroziak@gmail.com>
2020-02-15 11:57:58 +0100

    login.html prepared some neccessary changes in index.html

--- server.py
+++ server.py
@@ -16,5 +16,9 @@ def register():
     return render_template('register.html')
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
