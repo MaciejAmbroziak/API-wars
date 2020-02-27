3d638985010fb264d65d8303d3be9c1364c7df20
maciej <maciejambroziak@gmail.com>
2020-02-27 11:15:25 +0100

    residents and voing to be done

--- API_connection.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-
-response = requests.get('https://swapi.co/api/planets').json()
-dict_of_planets = response['results']
-for planet in dict_of_planets:
-    planet_data = [planet["name"], planet['diameter'], planet['climate'], planet['terrain'], planet['surface_water'],
-                   planet['population']]
--- server.py
+++ server.py
@@ -16,5 +16,10 @@ def register():
     return render_template('register.html')
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
