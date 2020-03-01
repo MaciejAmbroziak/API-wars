63fb5868d2e218d5aef7aad735e949982e6672d9
maciej <maciejambroziak@gmail.com>
2020-03-01 15:14:35 +0100

    modal almost ready - destroing data after ckick to be done

--- static/universe.js
+++ static/universe.js
@@ -1,20 +1,58 @@
+let residentsButtons = document.getElementsByClassName('btn btn-primary');
+let numberOfPlanetResidents = Array();
+let allResidents ='     <tr>\n' +
+    '                        <th>name</th>\n' +
+    '                        <th>height</th>\n' +
+    '                        <th>mass</th>\n' +
+    '                        <th>skin color</th>\n' +
+    '                        <th>hair color</th>\n' +
+    '                        <th>eye color</th>\n' +
+    '                        <th>birth year</th>\n' +
+    '                        <th>gender</th>\n' +
+    '                    </tr>';
 fetch('https://swapi.co/api/planets')  // set the path; the method is GET by default, but can be modified with a second parameter
 .then((response) => response.json())  // parse JSON format into JS object
 .then((data) => {
-    let residentsButtons = document.getElementsByClassName('residents');
-    let residentsOfPlanet = Array();
+    //for planet
     for (let i = 0; i < data.results.length; i++) {
-        residentsOfPlanet.push(data.results[i].residents.length);
-    }
-    for (let i = 0; i < residentsButtons.length; i++){
-        if (residentsOfPlanet[i] === 0) {
+        //pushing number of residents into array it is necessary for buttons residents buttons
+        numberOfPlanetResidents.push(data.results[i].residents.length);
+        //changing look of buttons
+        if (numberOfPlanetResidents[i] === 0) {
             residentsButtons[i].innerHTML = 'no known residents';
         } else {
-            residentsButtons[i].innerHTML = `${residentsOfPlanet[i]} resident(s)`;
-            residentsButtons[i].setAttribute('id',`planet-${i}`)
+            residentsButtons[i].innerHTML = `${numberOfPlanetResidents[i]} resident(s)`;
         }
         if (residentsButtons[i].innerHTML === 'no known residents') {
             residentsButtons[i].disabled = true;
         }
+        let planetResidents;
+        residentsButtons[i].addEventListener('click', function () {
+            for (let j = 0; j < data.results[i].residents.length; j++) {
+                if (data.results[i].residents.length > 0) {
+                    console.log(data.results[i].residents[j]);
+                    fetch(data.results[i].residents[j])
+                        .then((residentsResponse) => residentsResponse.json())
+                        .then((residentsData) => {
+                            planetResidents =
+                                `    
+                            <tr>
+                                <td>${residentsData.name}</td>
+                                <td>${residentsData.height}</td>
+                                <td>${residentsData.mass}</td>
+                                <td>${residentsData.skin_color}</td>
+                                <td>${residentsData.hair_color}</td>
+                                <td>${residentsData.eye_color}</td>
+                                <td>${residentsData.birth_year}</td>
+                                <td>${residentsData.gender}</td>
+                            </tr>
+                    `;
+                            allResidents = allResidents + planetResidents;
+                            document.getElementById("classTable").innerHTML = allResidents;
+                        });
+                }
+            }
+        });
     }
+
 });
