d5ae9666618d5835405cc0625f79103c4775582a
maciej <maciejambroziak@gmail.com>
2020-03-16 18:30:21 +0100

    get

--- server.py
+++ server.py
@@ -1,23 +1,27 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import requests
 import database
+
 app = Flask(__name__)
 
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-@app.route('/', methods=['POST', 'GET'])
+
+@app.route('/')
 def main():
     response = requests.get('https://swapi.co/api/planets').json()
     list_of_planets = response['results']
     planet_votes = database.check_number_of_votes()
     print(planet_votes)
     if session:
-        return render_template('planets.html', planets=list_of_planets, planet_votes=planet_votes, user=session['username'])
+        return render_template('planets.html', planets=list_of_planets, planet_votes=planet_votes,
+                               user=session['username'])
     else:
-        return render_template('planets.html', planets=list_of_planets, planet_votes=planet_votes, user='You are not logged in')
+        return render_template('planets.html', planets=list_of_planets, planet_votes=planet_votes,
+                               user='You are not logged in')
 
 
-@app.route('/register', methods=['GET','POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
         return render_template('register.html')
@@ -32,7 +36,7 @@ def register():
     return redirect(url_for('main'))
 
 
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('login.html')
@@ -45,7 +49,7 @@ def login():
         return redirect(url_for('main'))
 
 
-@app.route('/votes/<int:planet_id>/<planet_name>/<email>', methods = ['POST'])
+@app.route('/votes/<int:planet_id>/<planet_name>/<email>', methods=['POST'])
 def count_votes(planet_id, planet_name, email):
     user_id = 1
     database.insert_planet_to_vote_table(planet_id, planet_name, user_id)
@@ -56,7 +60,6 @@ def count_votes(planet_id, planet_name, email):
 def logout():
     session.pop('username', None)
     return redirect(url_for('main'))
-
 
 
 if __name__ == '__main__':
